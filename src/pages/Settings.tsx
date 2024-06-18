a31c1d36ba3b8e72c1cd596f9d5c8f87ab051df2
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-18 15:03:24 -0400

    Add dynamic sidebar to settings page
    The settings page has been enhanced with a dynamic sidebar. This new feature includes different sections such as "General", "Connections", and "About". It uses the useParams hook from react-router, allowing us to dynamically display the content and highlight the active tab.

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,37 @@
-export default function Settings(){
+import {Divider, Listbox, ListboxItem} from "@nextui-org/react";
+import {useParams} from "react-router-dom";
+
+export default function Settings()
+{
+    const tab = useParams().tab ?? "general";
     return (
-        <div>
-            <h1>Settings</h1>
+        <div className={"flex flex-row gap-3 h-full my-16 mx-4"}>
+            <Sidebar tab={tab}/>
+            <Divider orientation={"vertical"}/>
+            <SettingsContent tab={tab}/>
+        </div>
+    );
+}
+
+function Sidebar(props: { tab?: string })
+{
+    return (
+        <div className={"flex flex-col w-[300px]"}>
+            <h1 className={"text-2xl font-bold"}>Settings</h1>
+            <Listbox>
+                <ListboxItem key={"general"} href={"/settings"} className={`${props.tab === "general" ? "bg-primary/50 hover:!bg-primary transition-all" : ""} py-3 my-1`}>General</ListboxItem>
+                <ListboxItem key={"connections"} href={"/settings/connections"} className={`${props.tab === "connections" ? "bg-primary/50 hover:!bg-primary transition-all" : ""} py-3 my-1`}>Connections</ListboxItem>
+                <ListboxItem key={"about"} href={"/settings/about"} className={`${props.tab === "about" ? "bg-primary/50 hover:!bg-primary transition-all" : ""} py-3 my-1`}>About</ListboxItem>
+            </Listbox>
+        </div>
+    );
+}
+
+function SettingsContent(props: { tab?: string })
+{
+    return (
+        <div className={"w-full"}>
+            <h1>{props.tab}</h1>
         </div>
     );
 }
