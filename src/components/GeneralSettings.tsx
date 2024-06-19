aca5a9d6cb779de594aa35556886ced6897995b4
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-06-19 18:38:42 -0400

    Refactor GeneralSettings component formatting and interaction
    Applied prettier code formatting on GeneralSettings component for ease of readability. Enhanced interactivity by updating the default selected key in the 'Left Panel' based on settings and allowing changes to be saved. This enhancement increases the application's user-friendliness significantly.

--- src/components/GeneralSettings.tsx
+++ src/components/GeneralSettings.tsx
@@ -4,27 +4,34 @@ import SwitchOption from "./SwitchSetting.tsx";
 import {applyTheme, Themes} from "../assets/ts/Theme.ts";
 import {Select, SelectItem} from "@nextui-org/react";
 
-export default function General()
-{
+export default function General() {
     const [settings, setSettings] = useState<AppSettings>(currentSettings);
     return (
         <div className={"flex flex-col"}>
             <h1 className={"text-4xl mb-4"}>General</h1>
             <div className={"flex gap-3 sm:flex-col lg:flex-row mb-10"}>
-                <SwitchOption label={"Start with Windows"} description={"Should SFTP Editor start with Windows"} onToggle={value =>
-                {
+                <SwitchOption label={"Start with Windows"} description={"Should SFTP Editor start with Windows"} onToggle={value => {
                     setSettings({...settings, general_settings: {...settings?.general_settings, start_with_windows: value}});
                     SaveSettings({...settings, general_settings: {...settings?.general_settings, start_with_windows: value}});
                 }} selected={settings?.general_settings.start_with_windows ?? false}/>
-                <SwitchOption label={"Dark Mode"} description={"Enable or disable dark-mode"} onToggle={value =>
-                {
+                <SwitchOption label={"Dark Mode"} description={"Enable or disable dark-mode"} onToggle={value => {
                     applyTheme(value ? Themes.dark : Themes.light);
                     setSettings({...settings, general_settings: {...settings?.general_settings, dark_mode: value}});
-                }} selected={settings?.general_settings.dark_mode ?? false}/>
+                }} selected={settings?.general_settings?.dark_mode}/>
             </div>
             <h1 className={"text-2xl mb-4"}>Panels</h1>
             <div className={"flex gap-3 sm:flex-col lg:flex-row mb-3"}>
-                <Select defaultSelectedKeys={"2"} label={"Left Panel"}>
+                <Select defaultSelectedKeys={settings.general_settings.panel_settings.left.content.toString()} label={"Left Panel"} onSelectionChange={keys => {
+                    const key = Number.parseInt([...keys][0] as string);
+                    console.log(typeof key)
+
+                    settings.general_settings.panel_settings.left.content = key;
+                    setSettings(settings);
+                    console.log("Left Panel", settings.general_settings.panel_settings.left);
+                    console.log("Settings", settings);
+                    SaveSettings(settings);
+                }
+                }>
                     <SelectItem key={"0"}>None</SelectItem>
                     <SelectItem key={"1"}>Remote Filesystem</SelectItem>
                     <SelectItem key={"2"}>Local Filesystem</SelectItem>
@@ -56,5 +63,6 @@ export default function General()
                 </Select>
             </div>
         </div>
-    );
+    )
+        ;
 }
