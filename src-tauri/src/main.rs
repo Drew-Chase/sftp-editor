3cae8063e80adab70fa33f12da93bd378b59d236
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-19 14:44:16 -0400

    Add new plugins and setup in main function
    The original greet command in the main file was commented out, new plugins (tauri_plugin_single_instance and tauri_plugin_window_state) were added and a setup function was implemented. The setup function includes window settings, such as shadow and decorations. A Payload struct was also created for use with the single-instance plugin.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
+        }))
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .setup(|app| {
+            use window_shadows::set_shadow;
+            let window = app.get_window("main").unwrap();
+            set_shadow(&window, true).unwrap();
+            window.set_decorations(false).unwrap();
+            window.show().unwrap();
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    args: Vec<String>,
+    cwd: String,
 }
