067c7db702e5216f5273a129935203006bba92ad
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-06-19 18:38:12 -0400

    Add std::env import in main.rs
    An import for the standard library's environment module has been added to the main.rs file. This change was necessary to utilize some environment-related features in the code.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+
 use tauri::Manager;
 
 use app_settings::{get_settings, save_settings};
