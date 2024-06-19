abe6d393f591fafd27b22d72357cc247a983d662
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-19 16:28:54 -0400

    Refactor settings management system
    The previous settings system was replaced with a newer, more generalized one in the Settings.ts file. It now includes better-structured interfaces such as Panel, PanelSettings, GeneralSettings, and AppSettings. The "GetSettings" and "SaveSettings" functions have been updated correspondingly for better functionality and consistency across the application. Also, an external library "@tauri-apps/api/tauri" is used for setting operations.

--- src/assets/ts/Settings.ts
+++ src/assets/ts/Settings.ts
@@ -1,35 +1,44 @@
-export interface SettingsProps
+import {invoke} from "@tauri-apps/api/tauri";
+
+export interface Panel
 {
-    general: GeneralProps;
-    connections: ConnectionsProps;
+    content: number;
+    width: number;
+    height: number;
+    visible: boolean;
 }
 
-export interface GeneralProps
+export interface PanelSettings
 {
-    startWithWindows: boolean;
-    darkMode: boolean;
+    left: Panel;
+    top: Panel;
+    right: Panel;
+    bottom: Panel;
 }
 
-export interface ConnectionsProps
+export interface GeneralSettings
 {
-
+    dark_mode: boolean;
+    start_with_windows: boolean;
+    panel_settings: PanelSettings;
 }
 
-export function GetSettings(): SettingsProps
+export interface AppSettings
 {
-    const settings: string | null = localStorage.getItem("settings");
-    console.log(settings)
-    return settings ? JSON.parse(settings) :
-        {
-            general: {
-                startWithWindows: false,
-                darkMode: true
-            },
-            connections: {}
-        };
+    general_settings: GeneralSettings;
 }
 
-export function SaveSettings(settings: SettingsProps)
+export let currentSettings: AppSettings = {"general_settings": {"dark_mode": true, "start_with_windows": false, "panel_settings": {"left": {"content": 0, "width": 0, "height": 0, "visible": false}, "top": {"content": 0, "width": 0, "height": 0, "visible": false}, "right": {"content": 0, "width": 0, "height": 0, "visible": false}, "bottom": {"content": 0, "width": 0, "height": 0, "visible": false}}}};
+
+export async function GetSettings(): Promise<AppSettings>
 {
-    localStorage.setItem("settings", JSON.stringify(settings));
+    const settings: AppSettings = await invoke("get_settings");
+    console.log("Settings Loaded", settings);
+    currentSettings = settings;
+    return settings;
+}
+
+export function SaveSettings(settings: AppSettings)
+{
+    invoke("save_settings", {settings: settings}).then(() => console.log("Settings saved"));
 }
