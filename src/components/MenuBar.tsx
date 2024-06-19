e65f5847ec3240a0dda615789d0f3e8c63353d76
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-19 14:43:01 -0400

    Update MenuBar with WindowChrome functionality
    The MenuBar component has been updated to import and implement WindowChrome functionality. Changes include adding event handlers for minimize, maximize and close actions on window chrome, as well as styling modifications for better visual experience.

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {Navbar, NavbarContent, NavbarItem} from "@nextui-org/navbar";
 import {Button, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, Kbd, NavbarMenuToggle} from "@nextui-org/react";
 import {MaximizeIcon, MinimizeIcon, XIcon} from "./Icons.tsx";
+import WindowChrome from "../assets/ts/WindowChrome.ts";
+import "../assets/scss/chrome.scss";
 
 
 export default function MenuBar()
@@ -9,8 +11,8 @@ export default function MenuBar()
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
     return (
-        <Navbar onMenuOpenChange={setIsMenuOpen} maxWidth={"full"} height={"32px"} className={"m-0"}>
-            <NavbarContent>
+        <Navbar id={"window-chrome"} onMenuOpenChange={setIsMenuOpen} maxWidth={"full"} height={"32px"} classNames={{base: "m-0", wrapper: "px-0"}}>
+            <NavbarContent id={"window-menu-bar"}>
                 <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden"/>
                 <NavbarItem>
                     <Dropdown>
@@ -87,11 +89,11 @@ export default function MenuBar()
                     </Dropdown>
                 </NavbarItem>
             </NavbarContent>
-            <NavbarContent className={"w-full"}></NavbarContent>
-            <NavbarContent justify={"end"} className={"gap-0"}>
-                <NavbarItem className={"m-0"}> <Button variant={"light"} size={"sm"} className={"max-w-[24px] h-6"}> <MinimizeIcon opacity={.5} size={12}/> </Button> </NavbarItem>
-                <NavbarItem className={"m-0"}> <Button variant={"light"} size={"sm"} className={"w-6 h-6"}> <MaximizeIcon opacity={.5} size={12}/> </Button> </NavbarItem>
-                <NavbarItem className={"m-0"}> <Button variant={"light"} size={"sm"} color={"danger"} className={"w-6 h-6"}> <XIcon opacity={.5} size={18}/> </Button> </NavbarItem>
+            <NavbarContent id={"window-drag-bar"} className={"w-full bg-transparent hover:bg-foreground/10 transition-[background] rounded-md"} data-tauri-drag-region></NavbarContent>
+            <NavbarContent id={"window-chrome-actions"} justify={"end"} className={"gap-0"}>
+                <NavbarItem className={"m-0"}> <Button variant={"light"} size={"sm"} className={"max-w-[24px] h-6"} onClick={() => WindowChrome.getInstance().toggleMinimize()}> <MinimizeIcon opacity={.5} size={12}/> </Button> </NavbarItem>
+                <NavbarItem className={"m-0"}> <Button variant={"light"} size={"sm"} className={"w-6 h-6"} onClick={() => WindowChrome.getInstance().toggleMaximize()}> <MaximizeIcon opacity={.5} size={12}/> </Button> </NavbarItem>
+                <NavbarItem className={"m-0"}> <Button variant={"light"} size={"sm"} color={"danger"} className={"w-6 h-6"} onClick={() => WindowChrome.getInstance().close()}> <XIcon opacity={.5} size={18}/> </Button> </NavbarItem>
             </NavbarContent>
 
         </Navbar>
