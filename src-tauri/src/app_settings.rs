1ae15ef0f295f9d9b99422366bae9aa1681739dc
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-06-19 18:38:03 -0400

    Update Panel dimensions and improve settings file path handling
    The Panel's width and height types have been changed from i16 to f32 and their default values have been updated. The settings file path is now dynamically determined based on the executable's location, falling back to a default path when necessary. The env module has also been imported to facilitate this.

--- src-tauri/src/app_settings.rs
+++ src-tauri/src/app_settings.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::fs::File;
 
 use serde::{Deserialize, Serialize};
@@ -25,8 +26,8 @@ pub struct PanelSettings {
 #[derive(Serialize, Deserialize)]
 pub struct Panel {
     content: i16,
-    width: i16,
-    height: i16,
+    width: f32,
+    height: f32,
     visible: bool,
 }
 
@@ -36,41 +37,41 @@ const DEFAULT_SETTINGS: AppSettings = AppSettings {
         start_with_windows: false,
         panel_settings: PanelSettings {
             left: Panel {
-                content: 0,
-                width: 0,
-                height: 0,
+                content: 2,
+                width: 50f32,
+                height: 100f32,
                 visible: false,
             },
             top: Panel {
                 content: 0,
-                width: 0,
-                height: 0,
+                width: 100f32,
+                height: 15f32,
                 visible: false,
             },
             right: Panel {
                 content: 0,
-                width: 0,
-                height: 0,
+                width: 50f32,
+                height: 100f32,
                 visible: false,
             },
             bottom: Panel {
                 content: 0,
-                width: 0,
-                height: 0,
+                width: 100f32,
+                height: 15f32,
                 visible: false,
             },
         },
     },
 };
-const FILE_PATH: &str = "settings.json";
 
 #[tauri::command()]
 pub fn get_settings() -> AppSettings {
     use std::io::Read;
+    let file = get_settings_path();
 
-    let mut file = File::open(FILE_PATH).unwrap_or_else(|_| {
+    let mut file = File::open(&file).unwrap_or_else(|_| {
         save_settings(DEFAULT_SETTINGS);
-        File::open(FILE_PATH).expect("Failed to open file")
+        File::open(&file).expect("Failed to open file")
     });
 
     let mut serialized = String::new();
@@ -81,8 +82,29 @@ pub fn get_settings() -> AppSettings {
 #[tauri::command()]
 pub fn save_settings(settings: AppSettings) {
     use std::io::Write;
+    let file = get_settings_path();
 
     let serialized = serde_json::to_string_pretty(&settings).expect("Failed to serialize");
-    let mut file = File::create(FILE_PATH).expect("Failed to create file");
+    let mut file = File::create(file).expect("Failed to create file");
     file.write_all(serialized.as_bytes()).expect("Failed to write to file");
+}
+
+fn get_settings_path() -> String {
+    let exe_path = match env::current_exe() {
+        Ok(exe_path) => exe_path,
+        Err(e) => {
+            eprintln!("Failed to get current exe path: {}", e);
+            return "app_settings.json".to_string();
+        }
+    };
+
+    let exe_directory = match exe_path.parent() {
+        Some(dir) => dir,
+        None => {
+            eprintln!("Failed to get directory of the exe");
+            return "app_settings.json".to_string();
+        }
+    };
+
+    return exe_directory.join("app_settings.json").to_str().unwrap().to_string();
 }
