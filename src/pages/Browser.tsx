417137a7fe802d8446ca22762722c3e608fc7c4d
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-20 15:43:09 -0400

    Improve browser URL parameter handling
    This commit includes useParams from "react-router-dom" for obtaining URL parameters in the Browser.tsx page. If the parameter 'id' is not defined, the user will be redirected to a new browser page. Added a header to display the 'id'.

--- src/pages/Browser.tsx
+++ src/pages/Browser.tsx
@@ -1,7 +1,12 @@
+import {useParams} from "react-router-dom";
+
 export default function Browser()
 {
+    const {id} = useParams();
+    if (id === undefined) window.location.href = "/site-browser/new";
     return (
         <div>
+            <h1>Browser {id}</h1>
         </div>
     );
 }
