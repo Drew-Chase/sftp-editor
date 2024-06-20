479088725b5b5c5d02162d598abb8e7a9f638080
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-20 15:44:12 -0400

    Add Protocol enum and enhance Connection handling
    A new enum 'Protocol' has been introduced with types SFTP and FTP for a better handling of protocol types. Furthermore, the Connection object handling has been enhanced - an 'EmptyConnection' constant has been defined for easier object resetting, and incoming connection objects now have their date fields parsed into Date objects. Additionally, a utility function 'calculateTimeDifference' was added for time calculation purposes.

--- src/assets/ts/ConnectionManager.ts
+++ src/assets/ts/ConnectionManager.ts
@@ -1,5 +1,11 @@
 import {invoke} from "@tauri-apps/api/tauri";
 
+export enum Protocol
+{
+    SFTP = 0,
+    FTP = 1,
+}
+
 export interface Connection
 {
     id: number,
@@ -12,11 +18,29 @@ export interface Connection
     remote_path: string,
     local_path: string,
     default: boolean,
-    protocol: string,
+    protocol: Protocol,
     created_at: Date,
     updated_at: Date,
     last_connected_at: Date,
 }
+
+export const EmptyConnection: Connection = {
+    id: -1,
+    name: "",
+    host: "",
+    port: 22,
+    username: "",
+    password: "",
+    private_key: "",
+    remote_path: "",
+    local_path: "",
+    default: false,
+    protocol: Protocol.SFTP,
+    created_at: new Date(),
+    updated_at: new Date(),
+    last_connected_at: new Date()
+};
+
 
 export default class ConnectionManager
 {
@@ -43,6 +67,46 @@ export default class ConnectionManager
     async getConnections(): Promise<Connection[]>
     {
         this.connections = await invoke("get_connections") as Connection[];
+        this.connections = this.connections.map(connection =>
+        {
+            connection.created_at = new Date(connection.created_at);
+            connection.updated_at = new Date(connection.updated_at);
+            connection.last_connected_at = new Date(connection.last_connected_at);
+            return connection;
+        });
         return this.connections;
     }
+}
+
+export function calculateTimeDifference(date: Date): string
+{
+    const currentDate = new Date();
+
+    const totalSeconds: number = Math.floor((currentDate.getTime() - date.getTime()) / 1000);
+    const totalMinutes = Math.floor(totalSeconds / 60);
+    const totalHours = Math.floor(totalMinutes / 60);
+    const days = Math.floor(totalHours / 24);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(days / 365);
+
+    const seconds = totalSeconds % 60;
+    const minutes = totalMinutes % 60;
+    const hours = totalHours % 24;
+    const remainingDays = days % 30; // Note: Assumes each month is 30 days
+    const remainingMonths = months % 12;
+    let time: string = "";
+
+    if (years > 0)
+        time += `${years}y `;
+    if (remainingMonths > 0)
+        time += `${remainingMonths}mon `;
+    if (remainingDays > 0)
+        time += `${remainingDays}d `;
+    if (hours > 0)
+        time += `${hours}h `;
+    if (minutes > 0)
+        time += `${minutes}m `;
+    if (seconds > 0)
+        time += `${seconds}s `;
+    return time;
 }
