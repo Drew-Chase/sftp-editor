536b33e986a22a4f3609b668a79cc7a4089c1bd6
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-20 12:50:07 -0400

    Add connection management in main.rs
    Added the connection_manager module and implemented its functions in the main function of main.rs. This includes initializing a connection, adding a connection, getting connections, deleting a connection, updating a connection, and updating a join. Also, updated the Tauri builder to handle these new connection management functions.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,14 +6,41 @@ use std::env;
 use tauri::Manager;
 
 use app_settings::{get_settings, save_settings};
+use connection_manager::{add_connection, delete_connection, get_connections, initialize, update_connection, update_join};
+
+use crate::connection_manager::Connection;
 
 mod app_settings;
+mod connection_manager;
 
 fn main() {
+    initialize();
+    add_connection(Connection {
+        id: 0,
+        name: "test".to_string(),
+        host: "localhost".to_string(),
+        port: 22,
+        username: "test".to_string(),
+        password: "test".to_string(),
+        private_key: "".to_string(),
+        remote_path: "/".to_string(),
+        local_path: "C:/".to_string(),
+        default: true,
+        protocol: "sftp".to_string(),
+        created_at: "".to_string(),
+        updated_at: "".to_string(),
+        last_connected_at: "".to_string(),
+    });
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             get_settings,
-            save_settings
+            save_settings,
+            get_connections,
+            add_connection,
+            delete_connection,
+            update_connection,
+            update_join
         ])
         .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
             app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
