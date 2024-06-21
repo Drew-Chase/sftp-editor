107bcf412c93c412d2a0b7311091028a2580a2c5
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-21 13:49:43 -0400

    Update GeneralSettings component to use async functions
    The GeneralSettings component has been updated to use asynchronous functions for toggling settings and setting panel content. These changes ensure that settings are saved and fetched appropriately from the backend before updating the local state, which improves the consistency of data across the application.

--- src/components/GeneralSettings.tsx
+++ src/components/GeneralSettings.tsx
@@ -1,35 +1,35 @@
 import {useState} from "react";
-import {AppSettings, currentSettings, SaveSettings} from "../assets/ts/Settings.ts";
+import {AppSettings, currentSettings, GetSettings, SaveSettings} from "../assets/ts/Settings.ts";
 import SwitchOption from "./SwitchSetting.tsx";
 import {applyTheme, Themes} from "../assets/ts/Theme.ts";
 import {Select, SelectItem} from "@nextui-org/react";
 
-export default function General() {
+export default function General()
+{
     const [settings, setSettings] = useState<AppSettings>(currentSettings);
     return (
         <div className={"flex flex-col"}>
             <h1 className={"text-4xl mb-4"}>General</h1>
             <div className={"flex gap-3 sm:flex-col lg:flex-row mb-10"}>
-                <SwitchOption label={"Start with Windows"} description={"Should SFTP Editor start with Windows"} onToggle={value => {
+                <SwitchOption label={"Start with Windows"} description={"Should SFTP Editor start with Windows"} onToggle={async value =>
+                {
+                    await SaveSettings({...settings, general_settings: {...settings?.general_settings, start_with_windows: value}});
                     setSettings({...settings, general_settings: {...settings?.general_settings, start_with_windows: value}});
-                    SaveSettings({...settings, general_settings: {...settings?.general_settings, start_with_windows: value}});
                 }} selected={settings?.general_settings.start_with_windows ?? false}/>
-                <SwitchOption label={"Dark Mode"} description={"Enable or disable dark-mode"} onToggle={value => {
+                <SwitchOption label={"Dark Mode"} description={"Enable or disable dark-mode"} onToggle={async value =>
+                {
                     applyTheme(value ? Themes.dark : Themes.light);
+                    await GetSettings();
                     setSettings({...settings, general_settings: {...settings?.general_settings, dark_mode: value}});
                 }} selected={settings?.general_settings?.dark_mode}/>
             </div>
             <h1 className={"text-2xl mb-4"}>Panels</h1>
             <div className={"flex gap-3 sm:flex-col lg:flex-row mb-3"}>
-                <Select defaultSelectedKeys={settings.general_settings.panel_settings.left.content.toString()} label={"Left Panel"} onSelectionChange={keys => {
-                    const key = Number.parseInt([...keys][0] as string);
-                    console.log(typeof key)
-
-                    settings.general_settings.panel_settings.left.content = key;
+                <Select defaultSelectedKeys={settings.general_settings.panel_settings.left.content.toString()} label={"Left Panel"} onSelectionChange={async keys =>
+                {
+                    settings.general_settings.panel_settings.left.content = Number.parseInt([...keys][0] as string);
+                    await SaveSettings(settings);
                     setSettings(settings);
-                    console.log("Left Panel", settings.general_settings.panel_settings.left);
-                    console.log("Settings", settings);
-                    SaveSettings(settings);
                 }
                 }>
                     <SelectItem key={"0"}>None</SelectItem>
@@ -38,7 +38,13 @@ export default function General() {
                     <SelectItem key={"3"}>Local Terminal</SelectItem>
                     <SelectItem key={"4"}>Remote Terminal</SelectItem>
                 </Select>
-                <Select defaultSelectedKeys={"1"} label={"Right Panel"}>
+                <Select defaultSelectedKeys={settings.general_settings.panel_settings.right.content.toString()} label={"Right Panel"} onSelectionChange={async keys =>
+                {
+                    settings.general_settings.panel_settings.right.content = Number.parseInt([...keys][0] as string);
+                    await SaveSettings(settings);
+                    setSettings(settings);
+                }
+                }>
                     <SelectItem key={"0"}>None</SelectItem>
                     <SelectItem key={"1"}>Remote Filesystem</SelectItem>
                     <SelectItem key={"2"}>Local Filesystem</SelectItem>
@@ -46,15 +52,27 @@ export default function General() {
                     <SelectItem key={"4"}>Remote Terminal</SelectItem>
                 </Select>
             </div>
-            <div className={"flex gap-3 sm:flex-col lg:flex-row"}>
-                <Select defaultSelectedKeys={"0"} label={"Top Panel"}>
+            <div className={"flex gap-3 sm:flex-col lg:flex-row mb-3"}>
+                <Select defaultSelectedKeys={settings.general_settings.panel_settings.top.content.toString()} label={"Top Panel"} onSelectionChange={async keys =>
+                {
+                    settings.general_settings.panel_settings.top.content = Number.parseInt([...keys][0] as string);
+                    await SaveSettings(settings);
+                    setSettings(settings);
+                }
+                }>
                     <SelectItem key={"0"}>None</SelectItem>
                     <SelectItem key={"1"}>Remote Filesystem</SelectItem>
                     <SelectItem key={"2"}>Local Filesystem</SelectItem>
                     <SelectItem key={"3"}>Local Terminal</SelectItem>
                     <SelectItem key={"4"}>Remote Terminal</SelectItem>
                 </Select>
-                <Select defaultSelectedKeys={"4"} label={"Bottom Panel"}>
+                <Select defaultSelectedKeys={settings.general_settings.panel_settings.bottom.content.toString()} label={"Bottom Panel"} onSelectionChange={async keys =>
+                {
+                    settings.general_settings.panel_settings.bottom.content = Number.parseInt([...keys][0] as string);
+                    await SaveSettings(settings);
+                    setSettings(settings);
+                }
+                }>
                     <SelectItem key={"0"}>None</SelectItem>
                     <SelectItem key={"1"}>Remote Filesystem</SelectItem>
                     <SelectItem key={"2"}>Local Filesystem</SelectItem>
