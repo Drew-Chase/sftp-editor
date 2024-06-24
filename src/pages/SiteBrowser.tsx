6a267aee2fd7b33b50f3d3de7bed7e64a674b8e4
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-06-23 20:07:39 -0400

    Update SiteBrowser page with improved interface
    The SiteBrowser page has been enhanced with significant modifications in the connection details section and addition of new functionalities. Notably, the page now includes file input for the private key and path location for new connections. The user experience is further improved by better populating the connection details and implementing an 'Add' button for new connections.

--- src/pages/SiteBrowser.tsx
+++ src/pages/SiteBrowser.tsx
@@ -3,6 +3,7 @@ import {useParams} from "react-router-dom";
 import ConnectionManager, {Connection, EmptyConnection, Protocol} from "../assets/ts/ConnectionManager.ts";
 import {useState} from "react";
 import SwitchOption from "../components/SwitchSetting.tsx";
+import FileInput from "../components/FileInput.tsx";
 
 const manager = new ConnectionManager();
 await manager.getConnections();
@@ -28,13 +29,13 @@ function Sidebar(props: { tab?: string, onSetTab: (tab: string) => void })
             <h1 className={"text-2xl font-bold mb-4"}>Site Browser</h1>
             <Listbox>
                 {manager.connections.map(connection =>
-                    (
-                        <ListboxItem key={connection.id} href={`/site-browser/${connection.id}`} description={`${connection.username}@${connection.host}:${connection.port}`} onClick={() => props.onSetTab(connection.id.toString())} className={`${props.tab === connection.id.toString() ? "bg-primary/75 hover:!bg-primary transition-all" : ""} py-3 my-1`}>{connection.name}{connection.default ? (<span className={"text-danger ml-2"}>*</span>) : (<></>)}</ListboxItem>
-                    )
+                                             (
+                                                 <ListboxItem key={connection.id} href={`/site-browser/${connection.id}`} description={`${connection.username}@${connection.host}:${connection.port}`} onClick={() => props.onSetTab(connection.id.toString())} className={`${props.tab === connection.id.toString() ? "bg-primary/75 hover:!bg-primary transition-all" : ""} py-3 my-1`}>{connection.name}{connection.default ? (<span className={"text-danger ml-2"}>*</span>) : (<></>)}</ListboxItem>
+                                             )
                 ) as any}
             </Listbox>
 
-            <Button color={"primary"} className={"absolute bottom-[10px] right-3"} href={"/site-browser/new"} onClick={() => props.onSetTab("new")}>+</Button>
+            <Button color={"primary"} variant={"ghost"} className={"absolute bottom-[10px] right-3"} href={"/site-browser/new"} onClick={() => props.onSetTab("new")}>+</Button>
         </div>
     );
 }
@@ -43,48 +44,69 @@ function SiteDetails(props: { connection: Connection })
 {
     let [connection, setConnection] = useState<Connection>(props.connection);
     if (connection.id !== props.connection.id) setConnection(props.connection);
-    console.log("Connection", props.connection);
+    const isNewConnection = connection.id === EmptyConnection.id;
     const uniqueHosts = manager.connections.reduce((acc, val) => acc.includes(val.host) ? acc : [...acc, val.host], [] as string[]);
     const uniqueUsernames = manager.connections.reduce((acc, val) => acc.includes(val.username) ? acc : [...acc, val.username], [] as string[]);
     const uniquePorts = manager.connections.reduce((acc, val) => acc.includes(val.port.toString()) ? acc : [...acc, val.port.toString()], [] as string[]).map(i => Number.parseInt(i));
+    const onSave = async () =>
+    {
+        console.log("Saving connection", connection);
+    };
     return (
-        <div className={"flex flex-col w-full gap-3 overflow-y-auto"}>
+        <div className={"flex flex-col w-full gap-3 overflow-y-auto relative pr-5 pb-10"}>
             <div className={"mb-5"}>
-                <h1 className={"text-4xl font-bold mb-3"}>{connection.name}</h1>
-                <div className={"flex flex-row"}>
-                    <p className={""}><span className={"opacity-30"}>Created:</span><br/><span className={"font-bold opacity-30"}>{connection.created_at.toDateString()}</span></p>
-                    <Divider orientation={"vertical"} className={"mx-5 my-auto h-6"}/>
-                    <p className={""}><span className={"opacity-30"}>Updated:</span><br/><span className={"font-bold opacity-30"}>{connection.updated_at.toDateString()}</span></p>
-                    <Divider orientation={"vertical"} className={"mx-5 my-auto h-6"}/>
-                    <p className={""}><span className={"opacity-30"}>Last Joined:</span><br/><span className={"font-bold opacity-30"}>{connection.last_connected_at.toDateString()}</span></p>
-                </div>
-            </div>
-            <SwitchOption label={"Default"} description={"This will automatically connect on startup."} selected={connection.default} onToggle={async value =>
-            {
-                const temp = {...connection, default: value};
-                if (value)
+                <Input classNames={{
+                    input: "text-4xl font-bold",
+                    label: "text-lg font-bold",
+                    description: "text-sm"
+                }} placeholder={isNewConnection ? "New Connection" : "Connection Name"} value={connection.name} variant={"underlined"} onValueChange={async value =>
                 {
-                    await manager.setDefault(connection);
-                } else
-                {
-                    await manager.updateConnection(temp);
+                    const temp: Connection = {...connection, name: value};
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
+                    setConnection(temp);
+                }}/>
+                {isNewConnection ? <Button color={"primary"} className={"fixed bottom-4 right-7 z-10"} onClick={onSave}>Add</Button> :
+                    <>
+                        <SwitchOption label={"Default"} description={"This will automatically connect on startup."} selected={connection.default} onToggle={async value =>
+                        {
+                            const temp: Connection = {...connection, default: value};
+                            if (!isNewConnection)
+                                if (value)
+                                {
+                                    await manager.setDefault(connection);
+                                } else
+                                {
+                                    await manager.updateConnection(temp);
+                                }
+                            setConnection(temp);
+                        }}/>
+                        <div className={`flex flex-row`}>
+                            <p className={""}><span className={"opacity-30"}>Created:</span><br/><span className={"font-bold opacity-30"}>{connection.created_at.toDateString()}</span></p>
+                            <Divider orientation={"vertical"} className={"mx-5 my-auto h-6"}/>
+                            <p className={""}><span className={"opacity-30"}>Updated:</span><br/><span className={"font-bold opacity-30"}>{connection.updated_at.toDateString()}</span></p>
+                            <Divider orientation={"vertical"} className={"mx-5 my-auto h-6"}/>
+                            <p className={""}><span className={"opacity-30"}>Last Joined:</span><br/><span className={"font-bold opacity-30"}>{connection.last_connected_at.toDateString()}</span></p>
+                        </div>
+                    </>
                 }
-                setConnection(temp);
-            }}/>
+            </div>
             <h2 className={"text-lg font-bold"}>Connection Details</h2>
             <div className={"flex flex-row gap-2"}>
                 <Autocomplete inputValue={connection.host} allowsCustomValue label={"Host"} description={"The hostname or ip address of the ftp/sftp server"} onSelectionChange={async key =>
                 {
                     if (key === "" || key === null || key === undefined)
                         return;
-                    const temp = {...connection, host: key as string};
-                    await manager.updateConnection(temp);
+                    const temp: Connection = {...connection, host: key as string};
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
                     setConnection(temp);
 
                 }} onValueChange={async value =>
                 {
-                    const temp = {...connection, host: value as string};
-                    await manager.updateConnection(temp);
+                    const temp: Connection = {...connection, host: value as string};
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
                     setConnection(temp);
                 }}>
                     {uniqueHosts.map(host => <AutocompleteItem key={host} value={host}>{host}</AutocompleteItem>)}
@@ -94,23 +116,26 @@ function SiteDetails(props: { connection: Connection })
                     if (key === "" || key === null || key === undefined)
                         return;
                     const temp = {...connection, port: Number.parseInt(key as string)};
-                    await manager.updateConnection(temp);
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
                     setConnection(temp);
                 }} onValueChange={async value =>
                 {
                     if (value === "" || value === null || value === undefined)
                         return;
                     value = value.replace(/[^0-9]/g, "");
-                    const temp = {...connection, port: Number.parseInt(value)};
-                    await manager.updateConnection(temp);
+                    const temp: Connection = {...connection, port: Number.parseInt(value)};
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
                     setConnection(temp);
                 }}>
                     {uniquePorts.map(port => <AutocompleteItem key={port} value={port}>{port}</AutocompleteItem>)}
                 </Autocomplete>
                 <Select label={"Protocol"} description={"The connection protocol that will be used."} defaultSelectedKeys={[connection.protocol === Protocol.SFTP ? "0" : "1"]} className={"w-[30%] min-w-[100px]"} onSelectionChange={async keys =>
                 {
-                    const temp = {...connection, protocol: Number.parseInt([...keys][0] as string)};
-                    await manager.updateConnection(temp);
+                    const temp: Connection = {...connection, protocol: Number.parseInt([...keys][0] as string)};
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
                     setConnection(temp);
                 }}>
                     <SelectItem key={"0"}>SFTP</SelectItem>
@@ -123,22 +148,63 @@ function SiteDetails(props: { connection: Connection })
                 {
                     if (key === "" || key === null || key === undefined)
                         return;
-                    const temp = {...connection, username: key as string};
-                    await manager.updateConnection(temp);
+                    const temp: Connection = {...connection, username: key as string};
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
                     setConnection(temp);
 
                 }} onValueChange={async value =>
                 {
-                    const temp = {...connection, username: value as string};
-                    await manager.updateConnection(temp);
+                    const temp: Connection = {...connection, username: value as string};
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
                     setConnection(temp);
                 }}>
                     {uniqueUsernames.map(username => <AutocompleteItem key={username} value={username}>{username}</AutocompleteItem>)}
                 </Autocomplete>
                 <Input label={"Password"} type={"password"} description={"The password to use for authentication"} value={connection.password} onChange={async e =>
                 {
-                    const temp = {...connection, password: e.target.value};
-                    await manager.updateConnection(temp);
+                    const temp: Connection = {...connection, password: e.target.value};
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
+                    setConnection(temp);
+                }}/>
+            </div>
+
+            <FileInput
+                label={"Private Key"}
+                description={"The private key to use for authentication"}
+                variant={"multiline"}
+                valueType={"contents"}
+                onChange={async content =>
+                {
+                    const temp: Connection = {...connection, private_key: content};
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
+                    setConnection(temp);
+                }}/>
+
+            <h2 className={"text-lg font-bold"}>Locations</h2>
+            <div className={"flex flex-row gap-2"}>
+                <FileInput
+                    label={"Local Path"}
+                    description={"The local path that will be navigated to when connected."}
+                    variant={"single-line"}
+                    valueType={"path"}
+                    type={"directory"}
+                    value={connection.local_path}
+                    onChange={async content =>
+                    {
+                        const temp: Connection = {...connection, local_path: content};
+                        if (!isNewConnection)
+                            await manager.updateConnection(temp);
+                        setConnection(temp);
+                    }}/>
+                <Input label={"Remote Path"} description={"The remote path that will be navigated to when connected."} value={connection.remote_path} onValueChange={async value =>
+                {
+                    const temp: Connection = {...connection, remote_path: value};
+                    if (!isNewConnection)
+                        await manager.updateConnection(temp);
                     setConnection(temp);
                 }}/>
             </div>
