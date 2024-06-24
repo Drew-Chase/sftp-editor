bbea287064618bca93da0d6d5362324ddafb0db3
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-06-23 21:43:37 -0400

    Add TrashIcon and ConnectIcon components
    Two new SVG icons have been added to the Icons.tsx file: TrashIcon and ConnectIcon. These components will take props for size, color, opacity, and additional CSS classes, allowing for easy adjustment of the icons' visual characteristics.

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -161,3 +161,37 @@ export function FolderIcon(props: IconProps)
         />
     </svg>);
 }
+
+export function TrashIcon(props: IconProps)
+{
+    return (<svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+        width={props.width ?? props.size ?? 24}
+        height={props.height ?? props.size ?? 24}
+        opacity={props.opacity ?? 1}
+        className={props.className}
+    >
+        <path
+            d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"
+            fill={props.color ?? "currentColor"}
+        />
+    </svg>);
+}
+
+export function ConnectIcon(props: IconProps)
+{
+    return (<svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+        width={props.width ?? props.size ?? 24}
+        height={props.height ?? props.size ?? 24}
+        opacity={props.opacity ?? 1}
+        className={props.className}
+    >
+        <path
+            d="M9.4 86.6C-3.1 74.1-3.1 53.9 9.4 41.4s32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 9.4 86.6zM256 416H544c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32s14.3-32 32-32z"
+            fill={props.color ?? "currentColor"}
+        />
+    </svg>)
+}
