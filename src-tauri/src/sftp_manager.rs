601ecec587df0c82be4b744a4f5289858680980c
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-25 09:20:09 -0400

    Refactor sftp_manager to improve error handling and session authentication
    The sftp_manager.rs file has been updated to improve error handling during session handshake and SFTP use. A better structure has also been introduced to tailor session authentication depending on whether a private key or password authentication method is being used. Other modifications include extracting the private key to a file and deleting this file after it's been used for authentication.

--- src-tauri/src/sftp_manager.rs
+++ src-tauri/src/sftp_manager.rs
@@ -1,36 +1,74 @@
+use std::env;
+use std::io::Write;
 use std::net::TcpStream;
+use std::path::PathBuf;
 
 use ssh2::Session;
 
-use crate::connection_manager::{Connection, Protocol};
+use crate::connection_manager::Connection;
 
 #[tauri::command()]
 pub fn test_connection(options: Connection) -> bool {
-    match options.protocol {
-        Protocol::Sftp => {
-            let tcp = TcpStream::connect(format!("{}:{}", options.host, options.port)).unwrap();
-            let mut session = Session::new().unwrap();
-            session.set_tcp_stream(tcp);
-            session.handshake().unwrap();
-            if (options.private_key != "") {
-                let password: Option<&str> =
-                    if options.password != "" {
-                        Some(&options.password)
-                    } else {
-                        None
-                    };
-
-                session.userauth_pubkey_memory(&options.username, None, &options.private_key, password).unwrap();
-            } else {
-                session.userauth_password(&options.username, &options.password).unwrap();
+    return match options.protocol {
+        0 => { // SFTP
+            let mut session = connect(options);
+            if let Err(e) = session.handshake() {
+                println!("Error: {:?}", e);
             }
-        }
-        Protocol::Ftp => {}
-    }
 
-    return false;
+            if let Err(error) = session.sftp()
+            {
+                println!("Error: {:?}", error);
+            }
+
+            if let Some(banner) = session.banner()
+            {
+                println!("Banner: {:?}", banner);
+            }
+
+            session.authenticated()
+        }
+        1 => { // FTP
+            false
+        }
+        _ => {
+            println!("Unknown protocol: {}", options.protocol);
+            return false;
+        }
+    };
 }
 
+fn connect(options: Connection) -> Session {
+    let tcp = TcpStream::connect(format!("{}:{}", options.host, options.port)).unwrap();
+    let mut session = Session::new().unwrap();
+    session.set_tcp_stream(tcp);
+    session.handshake().unwrap();
+    if options.private_key != "" {
+        let password: Option<&str> =
+            if options.password != "" {
+                Some(&options.password)
+            } else {
+                None
+            };
+        let private_key = extract_private_key_to_file(&options.private_key);
+        let private_key = private_key.as_path();
+
+        session.userauth_pubkey_file(&options.username, None, private_key, password).unwrap();
+        std::fs::remove_file(private_key).unwrap();
+    } else {
+        session.userauth_password(&options.username, &options.password).unwrap();
+    }
+
+    return session;
+}
+
+fn extract_private_key_to_file(private_key: &str) -> PathBuf {
+    let binding = env::current_exe().unwrap().parent().unwrap().join(".tmp");
+    let filepath = binding.to_str().unwrap();
+    let mut file = std::fs::File::create(&filepath).unwrap();
+    file.write_all(private_key.as_bytes()).unwrap();
+    return PathBuf::from(&filepath);
+}
 // pub fn list(path: &str, options: Connection) -> Vec<String> {}
 //
 // pub fn download(remote_path: &str, local_path: &str, options: Connection) -> bool {}
