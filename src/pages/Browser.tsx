2006ff18a9eb7454995b3977f8600759f4881453
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-27 10:10:18 -0400

    Update Browser.tsx to display file details
    The Browser.tsx file is updated with tables for file details, with the purpose of better visualization. Additionally, ConnectionManager has been integrated for direct connection by ID, and a list directory function has been added for retrieving files. The display part contains information such as filename, date modified, type, and size of the file.

--- src/pages/Browser.tsx
+++ src/pages/Browser.tsx
@@ -1,12 +1,38 @@
 import {useParams} from "react-router-dom";
+import {Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/react";
+import ConnectionManager from "../assets/ts/ConnectionManager.ts";
 
+const id = useParams().id ?? "";
+if (id === "") window.location.href = "/site-browser/new";
+const manager = new ConnectionManager();
+const connection = await manager.getConnectionById(Number.parseInt(id));
+const files = await manager.listDirectory("/", connection);
+console.log(files)
 export default function Browser()
 {
-    const {id} = useParams();
-    if (id === undefined) window.location.href = "/site-browser/new";
     return (
         <div>
-            <h1>Browser {id}</h1>
+            <Table shadow={"none"}>
+                <TableHeader>
+                    <TableColumn className={"w-full"}>Filename</TableColumn>
+                    <TableColumn>Date Modified</TableColumn>
+                    <TableColumn>Type</TableColumn>
+                    <TableColumn>Size</TableColumn>
+                </TableHeader>
+                <TableBody>
+                    {files.map(file =>
+                    {
+                        return (
+                            <TableRow>
+                                <TableCell>{file.path}</TableCell>
+                                <TableCell>{file.modified}</TableCell>
+                                <TableCell>{file.is_dir ? "Folder" : "File"}</TableCell>
+                                <TableCell>{file.size}</TableCell>
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
         </div>
     );
 }
