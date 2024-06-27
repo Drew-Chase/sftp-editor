42675476e83b53b16697f7c173fc049c19d60ac9
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-27 10:09:01 -0400

    Add 'get_connection_by_id' function to main.rs
    This commit includes 'get_connection_by_id' to the list of functions imported from the connection_manager module. This function will provide us with the ability to retrieve specific connections by their ID.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,7 @@ use std::env;
 use tauri::Manager;
 
 use app_settings::{get_settings, save_settings};
-use connection_manager::{add_connection, delete_connection, get_connections, initialize, set_default, update_connection, update_join};
+use connection_manager::{add_connection, delete_connection, get_connections, initialize, set_default, update_connection, update_join, get_connection_by_id};
 use sftp_manager::{list, test_connection};
 
 // use crate::connection_manager::create_tmp_connection;
@@ -40,6 +40,7 @@ fn main() {
             set_default,
             test_connection,
             list,
+            get_connection_by_id
         ])
         .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
             app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
