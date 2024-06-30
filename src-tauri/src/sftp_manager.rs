8dae5a0faa43c8480940f51d8490d39b53d85732
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-06-30 19:13:33 -0400

    Refactor SFTP connection and error handling
    Reworked the error handling around the SFTP connection, by using Results and match to leverage Rust's error handling instead of unwrapping errors. This update includes better handling of TCP connections and authentications, a more robust exception capture including checks on the TCP stream, session handshake, file and password authentication. Also enhanced directory reading to exclude "." and ".." directories.

--- src-tauri/src/sftp_manager.rs
+++ src-tauri/src/sftp_manager.rs
@@ -22,22 +22,28 @@ pub struct File {
 pub fn test_connection(options: Connection) -> bool {
     return match options.protocol {
         0 => { // SFTP
-            let mut session = connect(options);
-            if let Err(e) = session.handshake() {
-                println!("Error: {:?}", e);
-            }
+            return match connect(options) {
+                Ok(mut session) => {
+                    if let Err(e) = session.handshake() {
+                        println!("Error: {:?}", e);
+                    }
 
-            if let Err(error) = session.sftp()
-            {
-                println!("Error: {:?}", error);
-            }
+                    if let Err(error) = session.sftp()
+                    {
+                        println!("Error: {:?}", error);
+                    }
 
-            if let Some(banner) = session.banner()
-            {
-                println!("Banner: {:?}", banner);
+                    if let Some(banner) = session.banner()
+                    {
+                        println!("Banner: {:?}", banner);
+                    }
+                    session.authenticated()
+                }
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                    false
+                }
             }
-
-            session.authenticated()
         }
         1 => { // FTP
             false
@@ -49,31 +55,51 @@ pub fn test_connection(options: Connection) -> bool {
     };
 }
 
-fn connect(options: Connection) -> Session {
-    let tcp = TcpStream::connect(format!("{}:{}", options.host, options.port)).unwrap();
-    let mut session = Session::new().unwrap();
-    session.set_tcp_stream(tcp);
-    session.handshake().unwrap();
-    if options.private_key != "" {
-        let password: Option<&str> =
-            if options.password != "" {
-                Some(&options.password)
+fn connect(options: Connection) -> Result<Session, String> {
+    println!("Attempting to connect using {:?}", options);
+    return match TcpStream::connect(format!("{}:{}", options.host, options.port)) {
+        Ok(tcp) => {
+            let mut session = Session::new().unwrap();
+            session.set_tcp_stream(tcp);
+            session.handshake().unwrap();
+            if options.private_key != "" {
+                let password: Option<&str> =
+                    if options.password != "" {
+                        Some(&options.password)
+                    } else {
+                        None
+                    };
+                let private_key = extract_private_key_to_file(&options.private_key);
+                let private_key = private_key.as_path();
+
+                match session.userauth_pubkey_file(&options.username, None, private_key, password) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        std::fs::remove_file(private_key).unwrap();
+                        return Err(format!("Error: {:?}", e));
+                    }
+                }
+                std::fs::remove_file(private_key).unwrap();
             } else {
-                None
-            };
-        let private_key = extract_private_key_to_file(&options.private_key);
-        let private_key = private_key.as_path();
-
-        session.userauth_pubkey_file(&options.username, None, private_key, password).unwrap();
-        std::fs::remove_file(private_key).unwrap();
-    } else {
-        session.userauth_password(&options.username, &options.password).unwrap();
-    }
-
-    return session;
+                match session.userauth_password(&options.username, &options.password) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                        return Err(format!("Error: {:?}", e));
+                    }
+                }
+            }
+            Ok(session)
+        }
+        Err(e) => {
+            println!("Error: {:?}", e);
+            Err(format!("Error: {:?}", e))
+        }
+    };
 }
 
 fn extract_private_key_to_file(private_key: &str) -> PathBuf {
+    println!("Extracting private key to file");
     let binding = env::current_exe().unwrap().parent().unwrap().join(".tmp");
     let filepath = binding.to_str().unwrap();
     let mut file = std::fs::File::create(&filepath).unwrap();
@@ -83,27 +109,38 @@ fn extract_private_key_to_file(private_key: &str) -> PathBuf {
 #[tauri::command()]
 pub fn list(path: &str, options: Connection) -> Vec<File> {
     let mut files: Vec<File> = Vec::new();
-    let session = connect(options);
-    let sftp = session.sftp().unwrap();
-    match sftp.opendir(Path::new(path)) {
-        Ok(mut dir) => {
-            loop {
-                match dir.readdir() {
-                    Ok((path, stat)) => {
-                        files.push(File {
-                            path: path.to_str().unwrap().to_string(),
-                            is_dir: stat.is_dir(),
-                            size: stat.size.unwrap(),
-                            modified: stat.mtime.unwrap(),
-                            permissions: stat.perm.unwrap(),
-                            owner: stat.uid.unwrap(),
-                            group: stat.gid.unwrap(),
-                        });
+    match connect(options) {
+        Ok(session) => {
+            let sftp = session.sftp().unwrap();
+            match sftp.opendir(Path::new(path)) {
+                Ok(mut dir) => {
+                    loop {
+                        match dir.readdir() {
+                            Ok((path, stat)) => {
+                                // remove the . and .. directories
+                                let path_string = path.to_str().unwrap().to_string();
+                                if path_string == "." || path_string == ".." {
+                                    continue;
+                                }
+                                files.push(File {
+                                    path: path_string,
+                                    is_dir: stat.is_dir(),
+                                    size: stat.size.unwrap(),
+                                    modified: stat.mtime.unwrap(),
+                                    permissions: stat.perm.unwrap(),
+                                    owner: stat.uid.unwrap(),
+                                    group: stat.gid.unwrap(),
+                                });
+                            }
+                            Err(e) => {
+                                println!("Error: {:?}", e);
+                                break;
+                            }
+                        }
                     }
-                    Err(e) => {
-                        println!("Error: {:?}", e);
-                        break;
-                    }
+                }
+                Err(e) => {
+                    println!("Error: {:?}", e);
                 }
             }
         }
@@ -111,6 +148,7 @@ pub fn list(path: &str, options: Connection) -> Vec<File> {
             println!("Error: {:?}", e);
         }
     }
+
     return files;
 }
 // pub fn download(remote_path: &str, local_path: &str, options: Connection) -> bool {}
