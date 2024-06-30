bf5ff6a645f5f0220f86b68c59654dce410cd4b8
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-06-30 19:13:55 -0400

    Refactor Browser.tsx to improve file navigation
    Updated the Browser component to use React hooks for state management and life cycle control. Added an enhancement that enables clicking on directory rows to navigate into them. Enhanced error handling by introducing checks for empty connections. Also provided sticky header functionality for the Table component.

--- src/pages/Browser.tsx
+++ src/pages/Browser.tsx
@@ -1,18 +1,42 @@
 import {useParams} from "react-router-dom";
 import {Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/react";
-import ConnectionManager from "../assets/ts/ConnectionManager.ts";
+import ConnectionManager, {Connection, EmptyConnection, File} from "../assets/ts/ConnectionManager.ts";
+import {useEffect, useState} from "react";
 
-const id = useParams().id ?? "";
-if (id === "") window.location.href = "/site-browser/new";
-const manager = new ConnectionManager();
-const connection = await manager.getConnectionById(Number.parseInt(id));
-const files = await manager.listDirectory("/", connection);
-console.log(files)
 export default function Browser()
 {
+    const id = useParams().id ?? "";
+    if (id === "") window.location.href = "/site-browser/new";
+    const manager = new ConnectionManager();
+    let connection: Connection = EmptyConnection;
+    const [files, setFiles] = useState<File[]>([] as File[]);
+    manager.getConnectionById(Number.parseInt(id))
+        .then(con => connection = con)
+        .then(() => path = connection.remote_path);
+    let path = "/";
+
+    useEffect(() =>
+              {
+                  if (connection.id === EmptyConnection.id)
+                  {
+                      console.error(`Cannot list files for an empty connection!`, connection);
+                      return;
+                  }
+                  console.log("Loading path: ", path, ", Connection: ", connection)
+                  manager.listDirectory(path, connection)
+                      .then(files => setFiles(files))
+                      .then(() => console.log("Connection: ", connection, ", Files: ", files))
+                      .catch((e) =>
+                             {
+                                 console.error(e)
+                                 window.location.href = "/site-browser/new";
+                             });
+              }, [path])
     return (
         <div>
-            <Table shadow={"none"}>
+            <Table isHeaderSticky classNames={{
+                tbody: "max-h-96",
+            }}>
                 <TableHeader>
                     <TableColumn className={"w-full"}>Filename</TableColumn>
                     <TableColumn>Date Modified</TableColumn>
@@ -21,16 +45,22 @@ export default function Browser()
                 </TableHeader>
                 <TableBody>
                     {files.map(file =>
-                    {
-                        return (
-                            <TableRow>
-                                <TableCell>{file.path}</TableCell>
-                                <TableCell>{file.modified}</TableCell>
-                                <TableCell>{file.is_dir ? "Folder" : "File"}</TableCell>
-                                <TableCell>{file.size}</TableCell>
-                            </TableRow>
-                        );
-                    })}
+                               {
+                                   return (
+                                       <TableRow onClick={() =>
+                                       {
+                                           if (file.is_dir)
+                                           {
+                                               path = `${path}${file.path}/`;
+                                           }
+                                       }}>
+                                           <TableCell>{file.path}</TableCell>
+                                           <TableCell>{file.modified}</TableCell>
+                                           <TableCell>{file.is_dir ? "Folder" : "File"}</TableCell>
+                                           <TableCell>{file.size}</TableCell>
+                                       </TableRow>
+                                   );
+                               })}
                 </TableBody>
             </Table>
         </div>
