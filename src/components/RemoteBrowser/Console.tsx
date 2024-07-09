e960bf3bb735f29d5af791972e6178bfcb643671
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-07-09 10:51:32 -0400

    Add Console component to RemoteBrowser
    A new file named Console.tsx is introduced into the RemoteBrowser directory. This file contains the functional component, Console, returning a div to be used as a console UI in the application.

---
+++ src/components/RemoteBrowser/Console.tsx
@@ -0,0 +1,8 @@
+export default function Console()
+{
+    return (
+        <div className={"bg-[#101010] rounded-lg overflow-y-auto w-[calc(100%_-_3rem)] mx-auto mt-4 h-[25vh] min-h-[140px]"}>
+            <h1>Console</h1>
+        </div>
+    );
+}
