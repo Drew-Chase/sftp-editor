e11593ce3fe48b08de653b48a5913fd26e9ddb0b
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-10 16:28:12 -0400

    Adjust Console component width in RemoteBrowser
    The width value in the Console component of the RemoteBrowser was removed. Previously, it was subtracting a value (3rem) from 100%, but now it automatically adapts to its container. The change enhances the flexible layout of the UI.

--- src/components/RemoteBrowser/Console.tsx
+++ src/components/RemoteBrowser/Console.tsx
@@ -16,7 +16,7 @@ export default function Console({connection, manager}: { connection: Connection,
     }, [connection]);
 
     return (
-        <div className={"bg-[#101010] rounded-lg overflow-y-auto w-[calc(100%_-_3rem)] mx-auto mt-4 h-[25vh] min-h-[140px]"}>
+        <div className={"bg-[#101010] rounded-lg overflow-y-auto mt-4 h-[25vh] min-h-[140px]"}>
             <h1>Console</h1>
         </div>
     );
