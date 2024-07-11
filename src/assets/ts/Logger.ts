b224362fca15e0f11af697b1c0b89fd3ffe621bf
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-11 15:11:33 -0400

    Remove ColorCodes enum, add method documentation in Logger.ts
    The commit removes the unused ColorCodes enumeration from the Logger.ts file. Furthermore, documentation for all methods in the Logger class has been added. These changes make the Logger class clearer and reduce unnecessary bloat in the code.

--- src/assets/ts/Logger.ts
+++ src/assets/ts/Logger.ts
@@ -1,32 +1,8 @@
 import {invoke} from "@tauri-apps/api/tauri";
 
-export enum ColorCodes
-{
-    Reset = "\x1b[0m",
-    Bright = "\x1b[1m",
-    Dim = "\x1b[2m",
-    Underscore = "\x1b[4m",
-    Blink = "\x1b[5m",
-    Reverse = "\x1b[7m",
-    Hidden = "\x1b[8m",
-    FgBlack = "\x1b[30m",
-    FgRed = "\x1b[31m",
-    FgGreen = "\x1b[32m",
-    FgYellow = "\x1b[33m",
-    FgBlue = "\x1b[34m",
-    FgMagenta = "\x1b[35m",
-    FgCyan = "\x1b[36m",
-    FgWhite = "\x1b[37m",
-    BgBlack = "\x1b[40m",
-    BgRed = "\x1b[41m",
-    BgGreen = "\x1b[42m",
-    BgYellow = "\x1b[43m",
-    BgBlue = "\x1b[44m",
-    BgMagenta = "\x1b[45m",
-    BgCyan = "\x1b[46m",
-    BgWhite = "\x1b[47m"
-}
-
+/**
+ * Interface for the log message
+ */
 interface LogMessage
 {
     message: string;
@@ -36,6 +12,11 @@ interface LogMessage
 
 export default class Log
 {
+    /**
+     * Log a debug message
+     * @param message - The message to log
+     * @param args - The arguments to replace in the message
+     */
     static debug(message: string, ...args: any[])
     {
         const msg: LogMessage = this.buildMessage(message, args, "DEBUG");
@@ -43,6 +24,11 @@ export default class Log
         invoke("log", {message: msg.message, logType: 0, arguments: JSON.stringify(msg.args)});
     }
 
+    /**
+     * Log an info message
+     * @param message - The message to log
+     * @param args - The arguments to replace in the message
+     */
     static info(message: string, ...args: any[])
     {
         const msg: LogMessage = this.buildMessage(message, args, "INFO");
@@ -51,6 +37,11 @@ export default class Log
         invoke("log", {message: msg.message, logType: 1, arguments: JSON.stringify(msg.args)});
     }
 
+    /**
+     * Log a warning message
+     * @param message - The message to log
+     * @param args - The arguments to replace in the message
+     */
     static warn(message: string, ...args: any[])
     {
         const msg: LogMessage = this.buildMessage(message, args, "WARN");
@@ -58,6 +49,11 @@ export default class Log
         invoke("log", {message: msg.message, logType: 2, arguments: JSON.stringify(msg.args)});
     }
 
+    /**
+     * Log an error message
+     * @param message - The message to log
+     * @param args - The arguments to replace in the message
+     */
     static error(message: string, ...args: any[])
     {
         const msg: LogMessage = this.buildMessage(message, args, "ERROR");
@@ -65,6 +61,13 @@ export default class Log
         invoke("log", {message: msg.message, logType: 3, arguments: JSON.stringify(msg.args)});
     }
 
+    /**
+     * Build the log message
+     * @param message - The message to log
+     * @param args - The arguments to replace in the message
+     * @param type - The type of the log message
+     * @private
+     */
     private static buildMessage(message: string, args: any[], type: string): LogMessage
     {
         const includedIndexes: number[] = message.match(/{(\d+)}/g)?.map(i => parseInt(i.replace(/[{}]/g, ""))) ?? [];
