7d9f78dafc2ef69bf82f0b63da9f2dce33ede43f
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-11 14:40:55 -0400

    Refactor Sidebar component and update button behavior
    The Sidebar component in the ConnectionsList was refactored to remove the usage of 'props.manager' and switch to using 'ConnectionManager.instance'. The removal and connection actions of list items are now handled directly with ConnectionManager's methods. Also, matching color for tooltip and button for the "Delete" action was added for better user experience in visual feedback. The 'navigate' function from 'react-router-dom' was implemented to replace the window.location.href for better SPA navigation handling.

--- src/components/ConnectionsList/Sidebar.tsx
+++ src/components/ConnectionsList/Sidebar.tsx
@@ -1,14 +1,16 @@
 import {Button, Listbox, ListboxItem, Tooltip} from "@nextui-org/react";
 import {ConnectIcon, TrashIcon} from "../Icons.tsx";
 import ConnectionManager from "../../assets/ts/ConnectionManager.ts";
+import {useNavigate} from "react-router-dom";
 
-export default function Sidebar(props: { tab?: string, manager: ConnectionManager, onSetTab: (tab: string) => void })
+export default function Sidebar(props: { tab?: string, onSetTab: (tab: string) => void })
 {
+    const navigate = useNavigate();
     return (
         <div className={"flex flex-col min-w-[200px] w-[25%] max-w-[400px] relative overflow-y-auto"}>
             <h1 className={"text-2xl font-bold mb-4"}>Site Browser</h1>
             <Listbox aria-label={"List of connections."}>
-                {props.manager.connections.map(connection =>
+                {ConnectionManager.instance.getConnections().map(connection =>
                     (
                         <ListboxItem
                             key={connection.id}
@@ -34,16 +36,16 @@ export default function Sidebar(props: { tab?: string, manager: ConnectionManage
                                     {connection.default ? (<span className={"ml-2"}>*</span>) : (<></>)}
                                 </div>
                                 <div className={"flex flex-row"}>
-                                    <Tooltip content={"Delete"}>
-                                        <Button variant={"light"} className={"max-w-[24px] min-w-[24px] min-h-[24px] max-h-[24px] p-0"} onClick={async e =>
+                                    <Tooltip content={"Delete"} color={"danger"}>
+                                        <Button variant={"light"} color={"danger"} className={"max-w-[24px] min-w-[24px] min-h-[24px] max-h-[24px] p-0"} onClick={async e =>
                                         {
                                             e.stopPropagation();
-                                            await props.manager.removeConnection(connection);
+                                            await ConnectionManager.removeConnection(connection);
                                             window.location.reload();
                                         }}><TrashIcon opacity={.5} size={12}/></Button>
                                     </Tooltip>
                                     <Tooltip content={"Connect"}>
-                                        <Button variant={"light"} className={"max-w-[24px] min-w-[24px] min-h-[24px] max-h-[24px] p-0"} onClick={() => window.location.href = `/connection/${connection.id}`}>
+                                        <Button variant={"light"} className={"max-w-[24px] min-w-[24px] min-h-[24px] max-h-[24px] p-0"} onClick={() => navigate(`/connection/${connection.id}`)}>
                                             <ConnectIcon size={12} opacity={.5}/>
                                         </Button>
                                     </Tooltip>
