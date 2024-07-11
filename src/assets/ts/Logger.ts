8d4ff74c6edb554301bfd4b0bab2ac987e402c0c
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-11 14:41:41 -0400

    Improve logging message construction
    Introduced an interface, LogMessage, and a new private function buildMessage to streamline the process of creating log messages. This enhances the way log messages are formed and presented, making it easier to see the type of log and its parameters. The changes apply to debug, info, warn, and error log types.

--- src/assets/ts/Logger.ts
+++ src/assets/ts/Logger.ts
@@ -27,34 +27,58 @@ export enum ColorCodes
     BgWhite = "\x1b[47m"
 }
 
+interface LogMessage
+{
+    message: string;
+    type: string,
+    args: any[];
+}
+
 export default class Log
 {
     static debug(message: string, ...args: any[])
     {
-        const msg = `[DEBUG] ${message.replace(/{(\d+)}/g, (_, number) => `${ColorCodes.FgGreen}${JSON.stringify(args[number])}${ColorCodes.Reset}`)}`;
-        console.debug(msg);
-        invoke("log", {message: msg});
+        const msg: LogMessage = this.buildMessage(message, args, "DEBUG");
+        console.debug(`%c[${msg.type}] ${msg.message}`, "background: transparent; color: #bada55; font-size: 12px; padding: 2px 5px; border-radius: 5px;", ...msg.args);
+        invoke("log", {message: msg.message, logType: 0, arguments: JSON.stringify(msg.args)});
     }
 
     static info(message: string, ...args: any[])
     {
-        const msg = `${ColorCodes.FgBlue}[INFO]${ColorCodes.Reset} ${message.replace(/{(\d+)}/g, (_, number) => `${ColorCodes.FgGreen}${JSON.stringify(args[number])}${ColorCodes.Reset}`)}`;
-        console.info(msg);
-        invoke("log", {message: msg});
+        const msg: LogMessage = this.buildMessage(message, args, "INFO");
+        console.info(`%c[${msg.type}] ${msg.message}`, "background: #303f62; color: #45a1ff; font-size: 12px; padding: 2px 5px; border-radius: 5px;", ...msg.args);
+
+        invoke("log", {message: msg.message, logType: 1, arguments: JSON.stringify(msg.args)});
     }
 
     static warn(message: string, ...args: any[])
     {
-        const msg = `${ColorCodes.FgYellow}[WARN]${ColorCodes.Reset} ${message.replace(/{(\d+)}/g, (_, number) => `${ColorCodes.FgGreen}${JSON.stringify(args[number])}${ColorCodes.Reset}`)}`;
-        console.warn(msg);
-        invoke("log", {message: msg});
+        const msg: LogMessage = this.buildMessage(message, args, "WARN");
+        console.warn(`[${msg.type}] ${msg.message}`, ...msg.args);
+        invoke("log", {message: msg.message, logType: 2, arguments: JSON.stringify(msg.args)});
     }
 
     static error(message: string, ...args: any[])
     {
-        const msg = `${ColorCodes.FgRed}[ERROR]${ColorCodes.Reset} ${message.replace(/{(\d+)}/g, (_, number) => `${ColorCodes.FgGreen}${JSON.stringify(args[number])}${ColorCodes.Reset}`)}`;
-        console.error(msg);
-        invoke("log", {message: msg});
+        const msg: LogMessage = this.buildMessage(message, args, "ERROR");
+        console.error(`[${msg.type}] ${msg.message}`, ...msg.args);
+        invoke("log", {message: msg.message, logType: 3, arguments: JSON.stringify(msg.args)});
+    }
+
+    private static buildMessage(message: string, args: any[], type: string): LogMessage
+    {
+        const includedIndexes: number[] = message.match(/{(\d+)}/g)?.map(i => parseInt(i.replace(/[{}]/g, ""))) ?? [];
+        const notIncludedArgs = args.filter((_, index) => !includedIndexes.includes(index));
+        for (const index of includedIndexes)
+        {
+            if (args[index] === undefined)
+            {
+                Log.error(`Failed build log message: Argument at index {0} is undefined`, index);
+                return {} as LogMessage;
+            }
+        }
+
+        return {message: `${message.replace(/{(\d+)}/g, (_, number) => `${JSON.stringify(args[number])}`)}`, type, args: notIncludedArgs};
     }
 
 }
