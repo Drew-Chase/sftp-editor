5f1e19f29a7bb94c6a37cfbb27b47f0084f79185
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-11 14:39:47 -0400

    Refactor settings functions and Select items rendering
    The commit makes two main changes to the GeneralSettings component. Firstly, async/await is removed from SaveSettings and GetSettings functions which simplifies the code and increases readability. Secondly, it refactors how SelectItems for panels are rendered. Instead of hardcoding them, it dynamically maps through a ContentType object, significantly improving code maintainability.

--- src/components/Settings/GeneralSettings.tsx
+++ src/components/Settings/GeneralSettings.tsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {AppSettings, currentSettings, GetSettings, SaveSettings} from "../../assets/ts/Settings.ts";
+import {AppSettings, ContentType, currentSettings, GetSettings, SaveSettings} from "../../assets/ts/Settings.ts";
 import SwitchOption from "../SwitchSetting.tsx";
 import {applyTheme, Themes} from "../../assets/ts/Theme.ts";
 import {Select, SelectItem} from "@nextui-org/react";
@@ -13,13 +13,13 @@ export default function General()
             <div className={"flex gap-3 sm:flex-col lg:flex-row mb-10"}>
                 <SwitchOption label={"Start with Windows"} description={"Should SFTP Editor start with Windows"} onToggle={async value =>
                 {
-                    await SaveSettings({...settings, general_settings: {...settings?.general_settings, start_with_windows: value}});
+                    SaveSettings({...settings, general_settings: {...settings?.general_settings, start_with_windows: value}});
                     setSettings({...settings, general_settings: {...settings?.general_settings, start_with_windows: value}});
                 }} selected={settings?.general_settings.start_with_windows ?? false}/>
                 <SwitchOption label={"Dark Mode"} description={"Enable or disable dark-mode"} onToggle={async value =>
                 {
                     applyTheme(value ? Themes.dark : Themes.light);
-                    await GetSettings();
+                    GetSettings();
                     setSettings({...settings, general_settings: {...settings?.general_settings, dark_mode: value}});
                 }} selected={settings?.general_settings?.dark_mode}/>
             </div>
@@ -28,56 +28,67 @@ export default function General()
                 <Select defaultSelectedKeys={settings.general_settings.panel_settings.left.content.toString()} label={"Left Panel"} onSelectionChange={async keys =>
                 {
                     settings.general_settings.panel_settings.left.content = Number.parseInt([...keys][0] as string);
-                    await SaveSettings(settings);
+                    SaveSettings(settings);
                     setSettings(settings);
                 }
                 }>
-                    <SelectItem key={"0"}>None</SelectItem>
-                    <SelectItem key={"1"}>Remote Filesystem</SelectItem>
-                    <SelectItem key={"2"}>Local Filesystem</SelectItem>
-                    <SelectItem key={"3"}>Local Terminal</SelectItem>
-                    <SelectItem key={"4"}>Remote Terminal</SelectItem>
+                    {
+                        Object.values(ContentType).filter(i => typeof i === "string").map((value, index) =>
+                        {
+                            value = value.toString().split(/(?=[A-Z])/).join(" ");
+                            return <SelectItem key={index}>{value.toString()}</SelectItem>;
+                        })
+                    }
                 </Select>
                 <Select defaultSelectedKeys={settings.general_settings.panel_settings.right.content.toString()} label={"Right Panel"} onSelectionChange={async keys =>
                 {
                     settings.general_settings.panel_settings.right.content = Number.parseInt([...keys][0] as string);
-                    await SaveSettings(settings);
+                    SaveSettings(settings);
                     setSettings(settings);
                 }
                 }>
-                    <SelectItem key={"0"}>None</SelectItem>
-                    <SelectItem key={"1"}>Remote Filesystem</SelectItem>
-                    <SelectItem key={"2"}>Local Filesystem</SelectItem>
-                    <SelectItem key={"3"}>Local Terminal</SelectItem>
-                    <SelectItem key={"4"}>Remote Terminal</SelectItem>
+                    {
+                        Object.values(ContentType).filter(i => typeof i === "string").map((value, index) =>
+                        {
+                            value = value.toString().split(/(?=[A-Z])/).join(" ");
+                            return <SelectItem key={index}>{value.toString()}</SelectItem>;
+                        })
+                    }
                 </Select>
             </div>
             <div className={"flex gap-3 sm:flex-col lg:flex-row mb-3"}>
                 <Select defaultSelectedKeys={settings.general_settings.panel_settings.top.content.toString()} label={"Top Panel"} onSelectionChange={async keys =>
                 {
                     settings.general_settings.panel_settings.top.content = Number.parseInt([...keys][0] as string);
-                    await SaveSettings(settings);
+                    SaveSettings(settings);
                     setSettings(settings);
                 }
                 }>
-                    <SelectItem key={"0"}>None</SelectItem>
-                    <SelectItem key={"1"}>Remote Filesystem</SelectItem>
-                    <SelectItem key={"2"}>Local Filesystem</SelectItem>
-                    <SelectItem key={"3"}>Local Terminal</SelectItem>
-                    <SelectItem key={"4"}>Remote Terminal</SelectItem>
+                    {
+                        Object.values(ContentType).filter(i => typeof i === "string").map((value, index) =>
+                        {
+                            value = value.toString().split(/(?=[A-Z])/).join(" ");
+                            return <SelectItem key={index}>{value.toString()}</SelectItem>;
+                        })
+                    }
                 </Select>
-                <Select defaultSelectedKeys={settings.general_settings.panel_settings.bottom.content.toString()} label={"Bottom Panel"} onSelectionChange={async keys =>
-                {
-                    settings.general_settings.panel_settings.bottom.content = Number.parseInt([...keys][0] as string);
-                    await SaveSettings(settings);
-                    setSettings(settings);
-                }
-                }>
-                    <SelectItem key={"0"}>None</SelectItem>
-                    <SelectItem key={"1"}>Remote Filesystem</SelectItem>
-                    <SelectItem key={"2"}>Local Filesystem</SelectItem>
-                    <SelectItem key={"3"}>Local Terminal</SelectItem>
-                    <SelectItem key={"4"}>Remote Terminal</SelectItem>
+                <Select
+                    defaultSelectedKeys={settings.general_settings.panel_settings.bottom.content.toString()}
+                    label={"Bottom Panel"}
+                    onSelectionChange={async keys =>
+                    {
+                        settings.general_settings.panel_settings.bottom.content = Number.parseInt([...keys][0] as string);
+                        SaveSettings(settings);
+                        setSettings(settings);
+                    }
+                    }>
+                    {
+                        Object.values(ContentType).filter(i => typeof i === "string").map((value, index) =>
+                        {
+                            value = value.toString().split(/(?=[A-Z])/).join(" ");
+                            return <SelectItem key={index}>{value.toString()}</SelectItem>;
+                        })
+                    }
                 </Select>
             </div>
         </div>
