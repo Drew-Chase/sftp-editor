1699468250bac6f15b90cad7a3d3cf212d15ec13
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-12 15:53:57 -0400

    Add collapse feature to console panel
    The console panel's state and history handling are commented out, in favor of focusing toward adding a new collapse button feature. Now, a tooltip with a corresponding close panel button has been added to the console to allow users to minimize the console panel when it is not in use. This modifies the console's functionality and user interface for improved user interaction.

--- src/components/RemoteBrowser/Console.tsx
+++ src/components/RemoteBrowser/Console.tsx
@@ -1,13 +1,15 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {Connection} from "../../assets/ts/ConnectionManager.ts";
 import Log from "../../assets/ts/Logger.ts";
+import {Button, Tooltip} from "@nextui-org/react";
+import {ClosePanelIcon} from "../Icons.tsx";
 
 export default function Console({connection}: { connection: Connection })
 {
-    const [consoleText, setConsoleText] = useState<string[]>([]);
-    const [input, setInput] = useState<string>("");
-    const [history, setHistory] = useState<string[]>([]);
-    const [historyIndex, setHistoryIndex] = useState<number>(-1);
+    // const [consoleText, setConsoleText] = useState<string[]>([]);
+    // const [input, setInput] = useState<string>("");
+    // const [history, setHistory] = useState<string[]>([]);
+    // const [historyIndex, setHistoryIndex] = useState<number>(-1);
 
     useEffect(() =>
     {
@@ -16,7 +18,14 @@ export default function Console({connection}: { connection: Connection })
     }, [connection]);
 
     return (
-        <div className={"bg-[#101010] rounded-lg overflow-y-auto mt-4 h-[25vh] min-h-[140px]"}>
+        <div className={"bg-[#101010] rounded-lg overflow-y-auto mt-4 h-[25vh] min-h-[140px] relative"}>
+            <div className={"absolute right-1 top-1"}>
+                <Tooltip content={"Collapse the Console Panel"}>
+                    <Button variant={"light"} className={"max-w-4 min-w-4 w-4 rounded-small"}>
+                        <ClosePanelIcon opacity={.25} className={"rotate-[-90deg]"}/>
+                    </Button>
+                </Tooltip>
+            </div>
             <h1>Console</h1>
         </div>
     );
