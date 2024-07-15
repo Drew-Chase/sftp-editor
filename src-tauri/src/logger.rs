ea223a0a969b0e31029e22d8904e5cf96eb211b0
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-07-14 21:11:28 -0400

    Add get_log_history function to logger module
    This commit introduces a new function 'get_log_history' to the logger module. This function fetches log history from a database based on given parameters: start_date, end_date, limit, log_types, and an optional search term. The logs are sorted in descending order by their creation date.

--- src-tauri/src/logger.rs
+++ src-tauri/src/logger.rs
@@ -64,3 +64,47 @@ pub fn initialize_log_file() {
         Err(e) => eprintln!("Error creating log file: {}", e)
     }
 }
+
+#[tauri::command]
+pub fn get_log_history(start_date: &str, end_date: &str, limit: i32, log_types: Vec<i8>, search: Option<&str>) {
+    let mut query = "SELECT * FROM `logs` WHERE `created` BETWEEN ? AND ?".to_string();
+    let mut params = vec![start_date, end_date];
+    if log_types.len() > 0 {
+        query.push_str(" AND `type` IN (");
+        for (i, log_type) in log_types.iter().enumerate() {
+            query.push_str(&log_type.to_string());
+            if i < log_types.len() - 1 {
+                query.push_str(", ");
+            }
+        }
+        query.push_str(")");
+    }
+    if let Some(q) = search {
+        query.push_str(" AND `message` LIKE ? OR `arguments` LIKE ?");
+        params.push(q);
+        params.push(q);
+    }
+    query.push_str(" ORDER BY `created` DESC LIMIT ?");
+    let limit = limit.to_string();
+    let limit = limit.as_str();
+    params.push(limit);
+    match sqlite::open(std::env::var("LOG_FILE_PATH").unwrap()) {
+        Ok(connection) => {
+            match connection.prepare(query.as_str()) {
+                Ok(mut statement) => {
+                    loop {
+                        for (i, param) in params.iter().enumerate() {
+                            statement.bind((i + 1, param.to_owned())).unwrap();
+                        }
+                        if statement.next().unwrap() == State::Done
+                        {
+                            break;
+                        }
+                    }
+                }
+                Err(e) => eprintln!("Error preparing log statement: {}", e)
+            }
+        }
+        Err(e) => eprintln!("Error creating log file: {}", e)
+    }
+}
