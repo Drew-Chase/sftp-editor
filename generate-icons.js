20f3c9243914c4ee04a9114705e4af8676f01f09
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-07-14 21:09:30 -0400

    Integrate `icon-gen` into icon generation process
    The code now imports `icon-gen` and uses it to generate icons from the original source. In addition to scaling the icons using `ffmpeg`, `icon-gen` generates .ico and .icns formats with multiple size variants. Error handling is added for any failures in icon generation.

--- generate-icons.js
+++ generate-icons.js
@@ -1,6 +1,7 @@
 import * as child_process from "node:child_process";
 import * as path from "node:path";
 import {fileURLToPath} from "url";
+import generateIcon from "icon-gen";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,6 +10,17 @@ const original_icons = path.join(__dirname, "Logo.png");
 const icons = path.join(__dirname, "src-tauri", "icons");
 const sizes = [16, 32, 64, 128, 256, 512];
 const ffmpeg = path.join(__dirname, "node_modules", "ffmpeg-static", "ffmpeg");
-sizes.forEach((size) => {
-    child_process.execSync(`${ffmpeg} -y -i ${original_icons} -vf \"scale=${size}:${size}\" ${path.join(icons, `${size}x${size}.png`)}`);
-});
+sizes.forEach((size) =>
+              {
+                  child_process.execSync(`${ffmpeg} -y -i ${original_icons} -vf \"scale=${size}:${size}\" ${path.join(icons, `${size}x${size}.png`)}`);
+              });
+generateIcon(original_icons, icons, {
+    report: true,
+    ico: {sizes: [16, 32, 48, 64, 128, 256, 512], name: "icon"},
+    icns: {sizes: [16, 32, 48, 64, 128, 256, 512], name: "icon"}
+})
+    .catch((error) =>
+           {
+               console.error("Failed to generate icon.", error);
+           });
+
