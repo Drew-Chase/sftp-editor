acdd817427abe02c173cab65c7a12aae591f989d
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-17 16:23:33 -0400

    Add key prop to JsonHierarchyComponent
    A 'key' prop was added to the JsonHierarchyComponent to ensure unique key values are maintained for each generated AccordionItem in the map method. This helps in the optimal rendering of React components, especially when the list is dynamically changing.

--- src/components/LogView/JsonHierarchyComponent.tsx
+++ src/components/LogView/JsonHierarchyComponent.tsx
@@ -1,17 +1,17 @@
 import {Accordion, AccordionItem} from "@nextui-org/react";
 
-export default function JsonHierarchyComponent({content}: { content: any })
+export default function JsonHierarchyComponent({key, content}: {key:string, content: any })
 {
     const keys = Object.keys(content);
     const supportedTypes = ["number", "string", "boolean"];
     return (
-        <Accordion style={{backgroundColor: "rgba(0,0,0,0.2)"}}>
+        <Accordion key={key} style={{backgroundColor: "rgba(0,0,0,0.2)"}}>
             {
-                keys.map((key: string) =>
+                keys.map((k: string) =>
                 {
                     return (
-                        <AccordionItem startContent={<div className={"text-medium inline-flex"}><span className={"font-bold pr-2"}>{key}</span><div className={"truncate max-w-[200px] opacity-70"}>{content[key]}</div></div>}>
-                            {supportedTypes.includes(typeof content[key]) ? <span className={"text-medium pl-4"}><span className={"font-bold pr-2"}>{key}:</span>{content[key]}</span> : <JsonHierarchyComponent content={content[key]}/>}
+                        <AccordionItem startContent={<div className={"text-medium inline-flex"}><span className={"font-bold pr-2"}>{k}</span><div className={"truncate max-w-[200px] opacity-70"}>{content[k]}</div></div>}>
+                            {supportedTypes.includes(typeof content[k]) ? <span className={"text-medium pl-4"}><span className={"font-bold pr-2"}>{k}:</span>{content[k]}</span> : <JsonHierarchyComponent key={`${key}-${k}`} content={content[k]}/>}
                         </AccordionItem>
                     );
                 })
