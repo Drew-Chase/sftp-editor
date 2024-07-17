fa2446a296158485e9389370ed1d82d7b1024503
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-17 16:23:10 -0400

    Refactor Logger component to customize log types
    The Logger component has been refactored to allow for customization of log types. The filter for logs has been adjusted so that different log types can be shown or hidden according to user preference. Additionally, timestamps have been added to the log data for better readability and context.

--- src/components/LogView/Log.tsx
+++ src/components/LogView/Log.tsx
@@ -1,9 +1,9 @@
-import {default as Logger, LogHistoryRequest, LogMessage} from "../../assets/ts/Logger.ts";
+import {default as Logger, LogMessage, LogType} from "../../assets/ts/Logger.ts";
 import {useEffect, useState} from "react";
 import {Accordion, AccordionItem, Spinner} from "@nextui-org/react";
 import JsonHierarchyComponent from "./JsonHierarchyComponent.tsx";
 
-export default function Log({filter}: { filter: LogHistoryRequest })
+export default function Log({showDebug, showInfo, showWarn, showError, limit}: { showDebug: boolean, showInfo: boolean, showWarn: boolean, showError: boolean, limit: number })
 {
     const [logs, setLogs] = useState<LogMessage[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
@@ -12,18 +12,25 @@ export default function Log({filter}: { filter: LogHistoryRequest })
     useEffect(() =>
     {
         setLoading(true);
-        Logger.history(filter).then((res: LogMessage[]) =>
-        {
-            setLogs(res);
+        Logger.history({
+            types: [
+                showDebug ? 0 : -1,
+                showInfo ? 1 : -1,
+                showWarn ? 2 : -1,
+                showError ? 3 : -1
+            ].filter((type: number) => type !== -1) as LogType[],
+            limit: limit
+        }, (logs: LogMessage[]) =>{
+            setLogs(logs);
             setLoading(false);
         });
-    }, [filter]);
+    }, [showDebug, showInfo, showWarn, showError, limit]);
 
 
     return (
-        <div id={"log-view"} className={"rounded-md w-[calc(100vw_-_4rem)] h-[calc(100vh_-_200px)] mt-4 dark:bg-[#101010] bg-[#ccc] overflow-y-auto"}>
+        <div id={"log-view"} className={"rounded-md w-[calc(100vw_-_4rem)] h-[calc(100vh_-_230px)] mt-4 dark:bg-[#101010] bg-[#ccc] overflow-y-auto"}>
 
-            {loading && <Spinner size="lg" className={"m-auto"} title={"Loading Logs..."}/>}
+            {loading && <Spinner size="lg" className={"w-full h-full"} label={"Loading Logs..."}/>}
             {!loading && logs.map((log: LogMessage) =>
                 {
                     return (
@@ -74,15 +81,18 @@ export default function Log({filter}: { filter: LogHistoryRequest })
                                 </div>
                             }
                             >
-                                {log.args.length === 0 ? <span>No additional data</span> : log.args.map((arg: any, index: number) =>
-                                {
-                                    const supportedTypes = ["number", "string", "boolean"];
-                                    return (
-                                        <div key={index} className={"text-xs"}>
-                                            {supportedTypes.includes(typeof arg) ? arg : <JsonHierarchyComponent content={arg}/>}
-                                        </div>
-                                    );
-                                })}
+                                <>
+                                    <span className={"opacity-70"}>Timestamp:</span> <span>{log.created?.toLocaleString()}</span>
+                                    {log.args.length === 0 ? <span>No additional data</span> : log.args.map((arg: any, index: number) =>
+                                    {
+                                        const supportedTypes = ["number", "string", "boolean"];
+                                        return (
+                                            <div key={index} className={"text-xs"}>
+                                                {supportedTypes.includes(typeof arg) ? arg : <JsonHierarchyComponent key={log.id?.toString()??""} content={arg}/>}
+                                            </div>
+                                        );
+                                    })}
+                                </>
                             </AccordionItem>
                         </Accordion>);
 
