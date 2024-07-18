c6f88c7db8a700930fe41bd051eb024c04936be1
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-18 16:00:28 -0400

    Add LogViewer page and function
    A new log viewer page has been created, accompanied by a function 'PageContent'. Within this function, relevant components are imported, settings retrieved, theme applied, singletons initialized and default right-click menu disabled.

---
+++ src/logWindow.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {BrowserRouter} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import $ from "jquery";
+
+import "./assets/scss/index.scss";
+import {applyTheme} from "./assets/ts/Theme.ts";
+import {GetSettings} from "./assets/ts/Settings.ts";
+import KeyboardShortcuts from "./assets/ts/KeyboardShortcuts.ts";
+import LogViewer from "./pages/LogViewer.tsx";
+import Log from "./assets/ts/Logger.ts";
+import MenuBar from "./components/MenuBar.tsx";
+
+
+ReactDOM.createRoot($("#root")[0]!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <PageContent/>
+        </BrowserRouter>
+    </React.StrictMode>
+);
+
+
+function PageContent()
+{
+    GetSettings();
+    applyTheme();
+    // Initialize the singletons
+    KeyboardShortcuts.instance;
+    Log.initialize();
+
+    // Disable the default right-click context menu
+    $(document).on("contextmenu", (e) => e.preventDefault());
+
+    return (
+        <>
+            <MenuBar title={"Log Viewer"} hideMenu/>
+            <LogViewer/>
+        </>
+    );
+}
