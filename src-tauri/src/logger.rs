bf28b7de6863b01c3a411d70350b812323e40f02
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-19 12:05:59 -0400

    Add functions to open and close log window
    Added two new functionalities to the logger.rs file: opening and closing of the log window. The `open_log_window` function creates a new window, sets it up, and displays it. The `close_log_window` function finds an open log window and closes it, returning an error if no open log window is found.

--- src-tauri/src/logger.rs
+++ src-tauri/src/logger.rs
@@ -2,14 +2,16 @@ use std::path::Path;
 
 use serde::{Deserialize, Serialize};
 use sqlite::State;
+use tauri::Manager;
+use window_shadows::set_shadow;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LogMessage {
-    id: i64,
-    message: String,
-    log_type: i8,
-    args: String,
-    created: String,
+	id: i64,
+	message: String,
+	log_type: i8,
+	args: String,
+	created: String,
 }
 
 /// Logs a message with the given log type and saves it to a SQLite database.
@@ -27,28 +29,28 @@ pub struct LogMessage {
 /// ```
 #[tauri::command]
 pub fn log(message: &str, arguments: &str, log_type: i64) {
-    match sqlite::open(std::env::var("LOG_FILE_PATH").unwrap()) {
-        Ok(connection) => {
-            match connection.prepare("INSERT INTO `logs` (`type`, `message`, `arguments`) VALUES (?, ?, ?);") {
-                Ok(mut statement) => {
-                    loop {
-                        statement.bind((1, log_type)).unwrap();
-                        statement.bind((2, message)).unwrap();
-                        statement.bind((3, arguments)).unwrap();
-                        if statement.next().unwrap() == State::Done
-                        {
-                            break;
-                        }
-                    }
-                }
-                Err(e) => eprintln!("Error preparing log statement: {}", e)
-            }
-        }
-        Err(e) => eprintln!("Error creating log file: {}", e)
-    }
+	match sqlite::open(std::env::var("LOG_FILE_PATH").unwrap()) {
+		Ok(connection) => {
+			match connection.prepare("INSERT INTO `logs` (`type`, `message`, `arguments`) VALUES (?, ?, ?);") {
+				Ok(mut statement) => {
+					loop {
+						statement.bind((1, log_type)).unwrap();
+						statement.bind((2, message)).unwrap();
+						statement.bind((3, arguments)).unwrap();
+						if statement.next().unwrap() == State::Done
+						{
+							break;
+						}
+					}
+				}
+				Err(e) => eprintln!("Error preparing log statement: {}", e)
+			}
+		}
+		Err(e) => eprintln!("Error creating log file: {}", e)
+	}
 
 
-    println!("{}", message);
+	println!("{}", message);
 }
 
 /// Initializes the log file by creating a SQLite database and a log table
@@ -59,66 +61,88 @@ pub fn log(message: &str, arguments: &str, log_type: i64) {
 /// initialize_log_file();
 /// ```
 pub fn initialize_log_file() {
-    let file_name = "sftp-editor-client-log.db";
-    let temp_dir = std::env::temp_dir();
-    let temp_dir = Path::new(&temp_dir);
-    let file_path = temp_dir.join(file_name);
-    std::env::set_var("LOG_FILE_PATH", &file_path);
-    match sqlite::open(file_path) {
-        Ok(connection) => {
-            if let Err(e) = connection.execute("CREATE TABLE IF NOT EXISTS `logs` ('id' INTEGER PRIMARY KEY, 'type' TINYINT, 'message' TEXT, 'arguments' TEXT DEFAULT NULL, 'created' TIMESTAMP DEFAULT CURRENT_TIMESTAMP);")
-            {
-                eprintln!("Error creating log table: {}", e);
-            }
-        }
-        Err(e) => eprintln!("Error creating log file: {}", e)
-    }
+	let file_name = "sftp-editor-client-log.db";
+	let temp_dir = std::env::temp_dir();
+	let temp_dir = Path::new(&temp_dir);
+	let file_path = temp_dir.join(file_name);
+	std::env::set_var("LOG_FILE_PATH", &file_path);
+	match sqlite::open(file_path) {
+		Ok(connection) => {
+			if let Err(e) = connection.execute("CREATE TABLE IF NOT EXISTS `logs` ('id' INTEGER PRIMARY KEY, 'type' TINYINT, 'message' TEXT, 'arguments' TEXT DEFAULT NULL, 'created' TIMESTAMP DEFAULT CURRENT_TIMESTAMP);")
+			{
+				eprintln!("Error creating log table: {}", e);
+			}
+		}
+		Err(e) => eprintln!("Error creating log file: {}", e)
+	}
 }
 
 #[tauri::command]
 pub fn get_log_history(start_date: &str, end_date: &str, limit: i32, log_types: Vec<i8>, search: Option<&str>) -> Result<Vec<LogMessage>, String> {
-    let mut query: String = format!("SELECT * FROM `logs` WHERE `created` BETWEEN '{}' AND '{}'", start_date, end_date);
-    if log_types.len() > 0 {
-        query.push_str(" AND `type` IN (");
-        for (i, log_type) in log_types.iter().enumerate() {
-            query.push_str(&log_type.to_string());
-            if i < log_types.len() - 1 {
-                query.push_str(", ");
-            }
-        }
-        query.push_str(")");
-    }
-    if let Some(q) = search {
-        query.push_str(format!(" AND `message` LIKE %{search}% OR `arguments` LIKE %{search}%", search = q).as_str());
-    }
-    query.push_str(format!(" ORDER BY `created` DESC LIMIT {}", limit).as_str());
-    match sqlite::open(std::env::var("LOG_FILE_PATH").unwrap()) {
-        Ok(conn) => {
-            match conn.prepare(query) {
-                Ok(mut statement) => {
-                    let mut logs: Vec<LogMessage> = Vec::new();
-                    while let State::Row = statement.next().unwrap() {
-                        logs.push(LogMessage {
-                            id: statement.read::<i64, usize>(0).map_err(|e| format!("Failed to get column from database: {}", e.to_string()))?,
-                            log_type: statement.read::<i64, usize>(1).map_err(|e| format!("Failed to get column from database: {}", e.to_string()))? as i8,
-                            message: statement.read::<String, usize>(2).map_err(|e| format!("Failed to get column from database: {}", e.to_string()))?,
-                            args: statement.read::<String, usize>(3).map_err(|e| format!("Failed to get column from database: {}", e.to_string()))?,
-                            created: statement.read::<String, usize>(4).map_err(|e| format!("Failed to get column from database: {}", e.to_string()))?,
-                        });
-                    }
-                    return Ok(logs);
-                }
-                Err(e) => Err(format!("Error executing log query: {}", e))
-            }
-        }
-        Err(e) => Err(format!("Error creating log file: {}", e))
-    }
-    
-    // pub fn open_log_window(){
-    //     let window = app.get_window("logger").unwrap();
-    //     set_shadow(&window, true).unwrap();
-    //     window.set_decorations(false).unwrap();
-    //     window.show().unwrap();
-    // }
-    
+	let mut query: String = format!("SELECT * FROM `logs` WHERE `created` BETWEEN '{}' AND '{}'", start_date, end_date);
+	if log_types.len() > 0 {
+		query.push_str(" AND `type` IN (");
+		for (i, log_type) in log_types.iter().enumerate() {
+			query.push_str(&log_type.to_string());
+			if i < log_types.len() - 1 {
+				query.push_str(", ");
+			}
+		}
+		query.push_str(")");
+	}
+	if let Some(q) = search {
+		query.push_str(format!(" AND `message` LIKE %{search}% OR `arguments` LIKE %{search}%", search = q).as_str());
+	}
+	query.push_str(format!(" ORDER BY `created` DESC LIMIT {}", limit).as_str());
+	match sqlite::open(std::env::var("LOG_FILE_PATH").unwrap()) {
+		Ok(conn) => {
+			match conn.prepare(query) {
+				Ok(mut statement) => {
+					let mut logs: Vec<LogMessage> = Vec::new();
+					while let State::Row = statement.next().unwrap() {
+						logs.push(LogMessage {
+							id: statement.read::<i64, usize>(0).map_err(|e| format!("Failed to get column from database: {}", e.to_string()))?,
+							log_type: statement.read::<i64, usize>(1).map_err(|e| format!("Failed to get column from database: {}", e.to_string()))? as i8,
+							message: statement.read::<String, usize>(2).map_err(|e| format!("Failed to get column from database: {}", e.to_string()))?,
+							args: statement.read::<String, usize>(3).map_err(|e| format!("Failed to get column from database: {}", e.to_string()))?,
+							created: statement.read::<String, usize>(4).map_err(|e| format!("Failed to get column from database: {}", e.to_string()))?,
+						});
+					}
+					return Ok(logs);
+				}
+				Err(e) => Err(format!("Error executing log query: {}", e))
+			}
+		}
+		Err(e) => Err(format!("Error creating log file: {}", e))
+	}
 }
+
+
+#[tauri::command]
+pub async fn open_log_window(handle: tauri::AppHandle) -> Result<(), String> {
+	if handle.get_window("logger").is_some() {
+		return Err("Log window is already open".to_string());
+	}
+	let window = tauri::WindowBuilder::new(
+		&handle,
+		"logger",
+		tauri::WindowUrl::App("log-window.html".into())
+	).build().unwrap();
+	set_shadow(&window, true).unwrap();
+	window.set_decorations(false).unwrap();
+	window.show().unwrap();
+
+	Ok(())
+}
+
+#[tauri::command]
+pub fn close_log_window(handle: tauri::AppHandle) -> Result<(), String> {
+	match handle.get_window("logger") {
+		Some(window) => {
+			window.close().unwrap();
+			Ok(())
+		},
+		None => Err("Log window is not open".to_string())
+	}
+}
+
